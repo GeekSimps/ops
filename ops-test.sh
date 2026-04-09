d4bbcd82006235e11902973e9a7951cb9eefd7ff
GeekSimps <simphiwej03@gmail.com>
2026-04-09 23:32:18 +0200

    Fix formatting and command issues in ops-test.sh
    Updated ops-test.sh script to correct formatting and commands.

--- ops-test.sh
+++ ops-test.sh
@@ -21,49 +21,49 @@ Part A:
    qemu-system-x86_64 \
 	-enable-kvm \
 	-m 768 \
-	-drive file=nova-prime.qcow2, format=qcow2 \
+	-drive file=nova-prime.qcow2,format=qcow2 \
 	-cdrom alpine-virt-3.20.0-x86_64.iso \
-	-boot d \ 
+	-boot d \
 	-netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
 	-device e1000,netdev=net0 \
 	-nographic
 
-5. sudo lxc-create -n pulse-prime -t -- --dist alpine --release 3.20 --arch amd64
+5. sudo lxc-create -n pulse-prime -t download -- --dist alpine --release 3.20 --arch amd64
    sudo lxc-start -n pulse-prime
    sudo lxc-ls --fancy
    sudo lxc-attach -n pulse-prime
 	
 
-6. sudo lxc-attach ip addr add 10.0.0.2/24 eth0
-   sudo ip link set eth0 up 
+6. sudo lxc-attach -n pulse-prime -- ip addr add 10.0.0.2/24 dev eth0
+   sudo lxc-attach -n pulse-prime -- ip link set eth0 up 
    
-      qemu-system-x86_64 \
+   qemu-system-x86_64 \
 	-enable-kvm \
 	-m 768 \
-	-drive file=nova-prime.qcow2, format=qcow2 \
+	-drive file=nova-prime.qcow2,format=qcow2 \
 	-cdrom alpine-virt-3.20.0-x86_64.iso \
-	-boot d \ 
+	-boot d \
 	-netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
 	-device e1000,netdev=net0 \
 	-nographic
 
-    sudo ip addr add 10.0.0.3/24 eth0
-    sudo ip link set eth0 up
-    sudo lxc-attach -n pulse-prime ping 10.0.0.3
+    sudo ip addr add 10.0.0.3/24 dev br-horizon
+    sudo ip link set br-horizon up
+    sudo lxc-attach -n pulse-prime -- ping 10.0.0.3
   
 Part B:
 
 1. sudo virt-install --name orion-secundus --ram 1024 --vcpus 1 --disk path=/var/lib/libvirt/images/orion-secundus.qcow2,size=15,format=qcow2 --os-type linux --os-variant ubuntu22.04 --network network=default,model=virtio --graphics vnc --cdrom /var/lib/libvirt/boot/ubuntu.iso
 
-2.sudo lxc-create -n beam-secundus -t download \-- -d ubuntu -r focal -a amd64
-  sudo lxc-start -n beam-secundus
-  sudo lxc-attach -n beam-secundus
+2. sudo lxc-create -n beam-secundus -t download -- -d ubuntu -r focal -a amd64
+   sudo lxc-start -n beam-secundus
+   sudo lxc-attach -n beam-secundus
 
-3.virsh snapshot-create-as orion-secundus snap1
-  virt-clone --original orion-secundus --name orion-clone --auto-clone
+3. virsh snapshot-create-as orion-secundus snap1
+   virt-clone --original orion-secundus --name orion-clone --auto-clone
 
-4.sudo lxc-cgroup -n beam-secundus memory.limit_in_bytes 1536M
-  free -m
+4. sudo lxc-cgroup -n beam-secundus memory.limit_in_bytes 1536M
+   free -m
 
 Part C:
 
@@ -87,6 +87,3 @@ Part C:
    ip netns exec ns_test2 ip link set veth2 up
 
    ip netns exec ns_test1 ping 192.168.1.2
-
-
-	 
